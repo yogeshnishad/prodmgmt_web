5f2e00f9385c4310cbd3d697945e8bbccbfd6625
yogeshnishad <yogesh.0822@gmail.com>
2022-07-30 22:23:19 +0530

    Initial commit

---
+++ Default.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div align="center" style="margin-top:40px">
+      <table cellspacing="30px">
+      <tr>
+      <th>Product</th>
+      <th>Quantity</th>
+      </tr>
+      <tr>
+      <td>
+          <asp:DropDownList ID="DropDownList1" runat="server" 
+              DataSourceID="SqlDataSource1" DataTextField="Product" 
+              DataValueField="ProductId" 
+              onselectedindexchanged="DropDownList1_SelectedIndexChanged" 
+              AutoPostBack="True">
+             
+          </asp:DropDownList>
+          <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+              ConnectionString="<%$ ConnectionStrings:projectmgtConnectionString %>" 
+              SelectCommand="SELECT [Product], [ProductId] FROM [mstProduct]">
+          </asp:SqlDataSource>
+      </td>
+      <td><asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+      <asp:RegularExpressionValidator ID="RegularExpressionValidator1"
+    ControlToValidate="TextBox1" runat="server"
+    ErrorMessage="Only Numbers allowed"
+    ValidationExpression="\d+">
+</asp:RegularExpressionValidator></td>
+      <td>
+        <asp:Button ID="Button1" runat="server" Text="Submit" ForeColor="#CC3300" 
+              onclick="Button1_Click" />
+              
+        </td>
+      </tr>
+      </table>
+    </div>
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+        Width="859px">
+        <Columns>  
+                    <asp:BoundField HeaderText="ID" DataField="productId" />  
+                    <asp:BoundField HeaderText="Product" DataField="product"/>
+                    <asp:BoundField HeaderText="Available Qty" DataField="availableQuantity" />  
+                    <asp:BoundField HeaderText="Base Price" DataField="basePrice" />  
+                    <asp:BoundField HeaderText="Total" DataField="total" />  
+                </Columns>  
+    </asp:GridView>
+    <asp:SqlDataSource ID="SqlDataSource2" runat="server" 
+        ConnectionString="<%$ ConnectionStrings:projectmgtConnectionString %>" 
+        SelectCommand="SELECT productId, product, availableQuantity, basePrice, availableQuantity*basePrice as total FROM mstProduct">
+    </asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    const string connectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\usert\Documents\projectmgt.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True";
+    int selectedIndex = 0;
+    int selectedId = 1;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.refreshDataset();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        int newAddedQuantity = Int32.Parse(TextBox1.Text);
+        
+        int oldQuantity = Int32.Parse(GridView1.Rows[DropDownList1.SelectedIndex].Cells[2].Text);
+        int newQuantity = oldQuantity + newAddedQuantity;
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            using (SqlCommand cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = "UPDATE mstProduct set availableQuantity= @newQuantity WHERE productId = @productId";
+                cmd.Parameters.AddWithValue("@newQuantity", newQuantity);
+                cmd.Parameters.AddWithValue("@productId", Int32.Parse(DropDownList1.SelectedValue));
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+            }
+
+            this.refreshDataset();
+        }
+
+    }
+
+    private void refreshDataset()
+    {
+        //GridView1.AutoGenerateColumns = false;
+
+        SqlConnection con = new SqlConnection(connectionString);
+        SqlDataAdapter adapter;
+        DataSet ds = new DataSet();
+        try
+        {
+            //create query string(SELECT QUERY)
+            String query = "SELECT productId, product, availableQuantity, basePrice, availableQuantity*basePrice as total FROM mstProduct";
+            con.Open();
+            //Adapter bind to query and connection object
+            adapter = new SqlDataAdapter(query, con);
+            //fill the dataset
+            adapter.Fill(ds);
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
+        }
+        catch (Exception ex)
+        {
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            int selectedIndex = DropDownList1.SelectedIndex;
+            TextBox1.Text = GridView1.Rows[selectedIndex].Cells[2].Text;
+            this.selectedIndex = selectedIndex;
+            this.selectedId = Int32.Parse(GridView1.Rows[selectedIndex].Cells[0].Text);
+        }
+        catch (Exception ex) { }
+    }
+}
